7396d6066f7c9dbd6d49cfe36571eff1e1b64b18
rayk <rayk@techie.com>
2026-02-04 04:46:17 +0000

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,8 @@ class MenuItemGeolocatorDemo extends StatelessWidget {
 
   final String functionalTitle;
 
-  ({Position? position, String? errorString}) getLocation() async {
+  /*   not working, but contains
+  Future({Position? position, String? errorString}) getLocation() async {
     try {
       // Define LocationSettings
       LocationSettings locationSettings = const LocationSettings(
@@ -111,10 +112,54 @@ class MenuItemGeolocatorDemo extends StatelessWidget {
       return (position: null, errorSting: errorString);
     }
   }
+*/
+
+  // Determine the current position of the device.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.',
+      );
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final (Position position?, String errorString?) = getLocation();
+    //    final (Position position?, String errorString?) = getLocation();
+    const String charToRepeat = '1'; // todo: aaaaaaaaaaaaaaaa
+
+    var pos = _determinePosition();
+    developer.log('Info: (todo) current position is <$pos>');
 
     return Scaffold(
       appBar: appBarForStdFunctional(context, functionalTitle),
@@ -122,7 +167,10 @@ class MenuItemGeolocatorDemo extends StatelessWidget {
         margin: EdgeInsets.all(20),
         child: Row(
           children: [
-            ElevatedButton(onPressed: () {}, child: Text('todo 1111111111')),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('todo 20260203 ${charToRepeat * 10}'),
+            ),
           ],
         ),
         //        child: mainWidget,
